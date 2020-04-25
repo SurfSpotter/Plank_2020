e37d6b5a8aa60a070ef1a98b884b32d3e475e0fb
dur5t <mukasor@gmail.com>
2020-04-25 21:47:13 +0300

    Initial Commit

---
+++ Plank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plank
+//
+//  Created by Алексей Чигарских on 25.04.2020.
+//  Copyright © 2020 Алексей Чигарских. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
