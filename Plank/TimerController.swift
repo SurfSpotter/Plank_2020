70e30987836216d1ea21776623e9ab5a29eb66bd
dur5t <mukasor@gmail.com>
2020-05-07 09:19:24 +0300

    progress bar with pod

--- Plank/TimerController.swift
+++ Plank/TimerController.swift
@@ -7,66 +7,24 @@
 //
 
 import UIKit
+import MBCircularProgressBar
+
 
 @IBDesignable
 
 class TimerController: UIViewController {
 
- 
-    
-    let shapeLayer = CAShapeLayer()
+    @IBOutlet weak var progressBar: MBCircularProgressBarView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    // lets do image fo timer
-        
-        let imageOfTimer = UIImageView(image: UIImage(named: "blankCircle"))
-        imageOfTimer.frame.size = CGSize(width: 370, height: 370)
-        
-        imageOfTimer.center = view.center
-        imageOfTimer.contentMode = .scaleToFill
-        view.addSubview(imageOfTimer)
-        
-        
-        
-        // Do any additional setup after loading the view.
-    
-    
-    // Let's shape a circle!
-     
-     
-     let center = view.center
-     let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
-     shapeLayer.path = circularPath.cgPath
-     shapeLayer.strokeColor = UIColor.blue.cgColor
-     shapeLayer.lineWidth = 10
-     shapeLayer.strokeEnd = 0
-     shapeLayer.lineCap = .round
-     shapeLayer.fillColor = UIColor.clear.cgColor
-     
-     
-     
-     
-     // Track Line
-     
-     let trackLayer = CAShapeLayer()
-     trackLayer.path = circularPath.cgPath
-     trackLayer.strokeColor = UIColor.lightGray.cgColor
-     trackLayer.lineWidth = 10
-     trackLayer.lineCap = .round
-     trackLayer.fillColor = UIColor.clear.cgColor
-     
-     
-     
-     
-     view.layer.addSublayer(trackLayer)
-     view.layer.addSublayer(shapeLayer)
-    }
-    
-    
+   
+}
+
     override func viewWillLayoutSubviews() {
-        
+        progressBar.layer.zPosition = 5
     }
 }
+
