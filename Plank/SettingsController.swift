3da23fdc0b751f696fa4f5d6d9172431d951e8a4
dur5t <mukasor@gmail.com>
2020-05-09 23:55:56 +0300

    Custom slider 1.5

--- Plank/SettingsController.swift
+++ Plank/SettingsController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SettingsController: UIViewController {
 
-    
+    let sliderValueUD = UserDefaults.standard.integer(forKey: "secCorrection")
     
     
     @IBOutlet weak var diffSliderOut: UISlider!
@@ -22,28 +22,46 @@ class SettingsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        diffSliderOut.setValue(Float(sliderValueUD), animated: false)
+        diffSliderLaberOut.text = sliderValStringUD
     }
     
     
     
     override func viewWillLayoutSubviews() {
-        self.diffSliderThumble.frame = CGRect(x: 118.0, y: 0 , width: 36, height: 38)
-    
-        
+        self.diffSliderLaberOut.frame = CGRect(x: (106.0 + (Double(sliderValueUD)) * 0.85833333) , y: 36 , width: 85, height: 30)
+        self.diffSliderThumble.frame = CGRect(x: (118.0 + Double(sliderValueUD) * 0.85833333) , y: 0 , width: 36, height: 38)
     }
     
 
     @IBAction func diffSliderAct(_ sender: Any) {
         print(Int(diffSliderOut.value))
-        diffSliderLaberOut.text = "\(Int(diffSliderOut.value))" + "sec"
-        self.diffSliderLaberOut.frame = CGRect(x: (106.0 + (Double(diffSliderOut.value)) * 0.85833333) , y: 36 , width: 67, height: 30)
+        diffSliderLaberOut.text = sliderValString
+        self.diffSliderLaberOut.frame = CGRect(x: (106.0 + (Double(diffSliderOut.value)) * 0.85833333) , y: 36 , width: 85, height: 30)
         self.diffSliderThumble.frame = CGRect(x: (118.0 + Double(diffSliderOut.value) * 0.85833333) , y: 0 , width: 36, height: 38)
-        
+        UserDefaults.standard.set((Int(diffSliderOut.value)), forKey: "secCorrection")
         
       
     }
     
+// Made custom string for slider with + and - and localised measures
+    
+    var sliderValStringUD: String {
+        get { if sliderValueUD > 0 {
+            return "+\(sliderValueUD)" + " sec"
+            }
+        else {  return "\(sliderValueUD)" + " sec"}
+            }
+        }
+    
+    var sliderValString: String {
+        get { if (Int(diffSliderOut.value)) > 0 {
+            return "+\(Int(diffSliderOut.value))" + " sec"
+            }
+        else {  return "\(Int(diffSliderOut.value))" + " sec"}
+            }
+        }
 
+
+    
 }
